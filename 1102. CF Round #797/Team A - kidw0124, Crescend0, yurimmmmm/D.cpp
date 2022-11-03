65c6c989e42619e2edacb70b9df42fe78e05870b
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-11-03 12:28:17 +0900

    By MatKor: kidw0124, Crescend0, yurimmmmm#, contest: Codeforces Round #797 (Div. 3), problem: (D) Black and White Stripe, Accepted, #, Copy
    https: //codeforces.com/contest/1690/submission/178947702
    Co-Authored-By: apple8718 <66424984+Yeahoon-Kim@users.noreply.github.com>
    Co-Authored-By: Yurim Lee <101806773+yurimn@users.noreply.github.com>

---
+++ 1102. CF Round #797/Team A - kidw0124, Crescend0, yurimmmmm/D.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define rep(i,a,b) for (auto i = (a); i < (b); i++)
+#define each(x, a) for (auto& x: a)
+#define endl '\n'
+
+#define debug if constexpr (xdebug) cout << "[DEBUG] "
+#define debugv(x) if constexpr (xdebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugc(c) if constexpr (xdebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+#ifndef ONLINE_JUDGE
+constexpr bool xdebug = true;
+#else
+constexpr bool xdebug = false;
+#endif
+
+ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
+ll POW(ll a, ll b, ll rem){ll p=1;for(;b;b/=2,a=(a*a)% rem)if(b&1)p=(p*a)%rem;return p;}
+
+int main(){
+	ll i,j;
+	ll n;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	ll t;
+	cin>>t;
+	while(t--){
+        ll a,b;
+        cin>>a>>b;
+        string s;
+        cin>>s;
+        vector<ll>arr(a+1);
+        for(i=0;i<a;i++){
+            arr[i+1]=arr[i]+(s[i]=='W');
+        }
+        ll x=INT_MAX;
+        for(i=b;i<=a;i++){
+            x=min(x,max(0ll,arr[i]-arr[i-b]));
+        }
+        cout<<x<<'\n';
+	}
+	return 0;
+}
