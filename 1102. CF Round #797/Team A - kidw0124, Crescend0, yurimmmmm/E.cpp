025700fb23454cdf3bd0923256aa5dfc89e16d7a
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-11-03 12:29:36 +0900

    By MatKor: kidw0124, Crescend0, yurimmmmm#, contest: Codeforces Round #797 (Div. 3), problem: (E) Price Maximization, Wrong answer on test 3, #, Copy
    https://codeforces.com/contest/1690/submission/178948905
    * Probably, the solution is executed with error 'signed integer overflow' on the line 33
    
    Co-Authored-By: apple8718 <66424984+Yeahoon-Kim@users.noreply.github.com>
    Co-Authored-By: Yurim Lee <101806773+yurimn@users.noreply.github.com>

---
+++ 1102. CF Round #797/Team A - kidw0124, Crescend0, yurimmmmm/E.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define ull unsigned long long int
+#define lll __int128_t
+#define ld long double
+#define INF (int)1e9
+#define INFLL (long long)1e18
+#define pii pair<int, int>
+#define pll pair<long long int, long long int>
+#define all(x) (x).begin(), (x).end()
+#define MOD (INF + 7)
+
+using namespace std;
+
+void init();
+
+int main() {
+    init();
+
+    int testCase, presentNum, i, k, left, right, result;
+    vector<int> presents;
+
+    cin >> testCase;
+    while(testCase--) {
+        cin >> presentNum >> k;
+        result = 0;
+
+        presents.resize(presentNum);
+        for(i = 0; i < presentNum; i++) {
+            cin >> presents[i];
+
+            result += presents[i] / k;
+            presents[i] %= k;
+        }
+
+        sort(presents.begin(), presents.end());
+
+        left = 0, right = presentNum - 1;
+
+        while(left < right) {
+            if(presents[left] + presents[right] < k) left++;
+            else {
+                result++;
+
+                left++;
+                right--;
+            }
+        }
+
+        cout << result << '\n';
+    }
+
+
+    return 0;
+}
+
+void init(void) {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "rt+", stdin);
+    freopen("output.txt", "wt+", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
