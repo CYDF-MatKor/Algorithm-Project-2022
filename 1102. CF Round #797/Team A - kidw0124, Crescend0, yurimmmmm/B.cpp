2aaddd33ac1890c7132e085ee5ce459d940ee089
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-11-03 12:25:54 +0900

    By MatKor: kidw0124, Crescend0, yurimmmmm#, contest: Codeforces Round #797 (Div. 3), problem: (B) Array Decrements, Wrong answer on test 2, #, Copy
    https: //codeforces.com/contest/1690/submission/178946070
    Co-Authored-By: apple8718 <66424984+Yeahoon-Kim@users.noreply.github.com>
    Co-Authored-By: Yurim Lee <101806773+yurimn@users.noreply.github.com>

---
+++ 1102. CF Round #797/Team A - kidw0124, Crescend0, yurimmmmm/B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define ull unsigned long long int
+#define lll __int128_t
+#define ld long double
+#define INF (int)1e9
+#define INFLL (long long)1e18
+#define pii pair<int, int>
+#define pll pair<long long int, long long int>
+#define all(x) (x).begin(), (x).end()
+#define MOD (INF + 7)
+
+using namespace std;
+
+void init();
+
+int main() {
+    init();
+
+    int testCase, arrNum, i;
+    vector<int> arrA, arrB;
+    bool isPossible;
+
+    cin >> testCase;
+    while(testCase--) {
+        cin >> arrNum;
+        isPossible = true;
+
+        arrA.resize(arrNum);
+        arrB.resize(arrNum);
+
+        for(i = 0; i < arrNum; i++) cin >> arrA[i];
+        for(i = 0; i < arrNum; i++) cin >> arrB[i];
+
+        for(i = 0; i < arrNum; i++) {
+            if(arrA[i]<arrB[i])isPossible=0;
+            if(arrA[i] - arrB[i]==arrA[0]-arrB[0] || arrB[i]==0) continue;
+
+            isPossible = false;
+            break;
+        }
+
+        cout << (isPossible ? "YES\n" : "NO\n");
+    }
+
+    return 0;
+}
+
+void init(void) {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "rt+", stdin);
+    freopen("output.txt", "wt+", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
